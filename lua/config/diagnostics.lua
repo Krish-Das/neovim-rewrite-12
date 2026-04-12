3b6c785d119dc50ebd997fc3dc73302b913d4ef5
Krish-Das <krishdasatgames@gmail.com>
2026-04-12 03:55:00 +0000

    add diagnostic display configuration
    Hide virtual text by default and add a keybinding to toggle
    diagnostic virtual text for better focus during editing.

---
+++ lua/config/diagnostics.lua
@@ -0,0 +1,19 @@
+local map = require("config.keymap_utils")
+
+-- Disable virtual_text by default
+vim.diagnostic.config({
+  virtual_text = false,
+  virtual_lines = false,
+  signs = true,
+  underline = true,
+})
+
+-- Toggle virtual text
+map("<leader>td", function()
+  vim.diagnostic.config({
+    virtual_text = true,
+    virtual_lines = false, -- not vim.diagnostic.config().virtual_lines,
+    signs = true,
+    underline = true,
+  })
+end, "[T]oggle [D]iagnostics")
