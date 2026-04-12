415e0d546d83ba1619f7b8bdaaf9fcd16e7c2ba6
Krish-Das <krishdasatgames@gmail.com>
2026-04-12 05:01:51 +0000

    add miscellaneous plugins
    Add mini.nvim surround and AI, autotag for JSX/HTML, leap for
    motion, origami for folding, startup time profiler, and zen mode
    for distraction-free editing.

---
+++ lua/plugins/misc.lua
@@ -0,0 +1,73 @@
+local event = { "BufReadPost", "BufNewFile" }
+return {
+  -- Mini.nvim
+  { "nvim-mini/mini.surround", opts = {}, event = event },
+  { "nvim-mini/mini.ai", opts = {}, event = event },
+
+  -- Autotag
+  {
+    "windwp/nvim-ts-autotag",
+    ft = { "typescriptreact", "javascriptreact", "html" },
+    opts = {
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true, -- Auto close on trailing </
+      },
+      per_filetype = {
+        ["html"] = { enable_close = false },
+      },
+    },
+  },
+
+  -- Leap jump
+  {
+    "ggandor/leap.nvim",
+    keys = {
+      { "g>", "<Plug>(leap-forward-to)", desc = "Leap: Forward to", mode = { "n", "x", "o" } },
+      { "g<", "<Plug>(leap-backward-to)", desc = "Leap: Backward to", mode = { "n", "x", "o" } },
+      { "x", "<Plug>(leap-forward-till)", desc = "Leap: Forward till", mode = { "x", "o" } },
+      { "X", "<Plug>(leap-backward-till)", desc = "Leap: Backward till", mode = { "x", "o" } },
+      { "gs", "<Plug>(leap-from-window)", desc = "Leap: to non-current buffer", mode = { "n", "x", "o" } },
+    },
+  },
+
+  -- Fold plugin
+  {
+    "chrisgrieser/nvim-origami",
+    event = event,
+    opts = {
+      foldtext = {
+        lineCount = { template = " %d" },
+      },
+      autoFold = { enabled = false },
+      foldKeymaps = { setup = false },
+    },
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  },
+
+  -- Startup time
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    init = function()
+      -- Configuration for plugins that don't force you to call a `setup` function
+      -- for initialization should typically go in a `before`
+      --- or `beforeAll` function.
+      vim.g.startuptime_tries = 10
+    end,
+  },
+
+  -- Zen mode
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = { window = { width = 0.75, options = {
+      signcolumn = "no",
+      number = false,
+    } } },
+  },
+}
