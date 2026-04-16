3eef89cba7edeed329a4d12249ab1a718cd2c9f9
Krish-Das <krishdasatgames@gmail.com>
2026-04-16 08:13:46 +0530

    going over kickstart-nvim

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,69 @@
+local lsp = {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    {
+      "mason-org/mason.nvim",
+      ---@module 'mason.settings'
+      ---@type MasonSettings
+      ---@diagnostic disable-next-line: missing-fields
+      opts = {},
+    },
+    "mason-org/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+
+    -- Eye candy
+    { "j-hui/fidget.nvim", opts = {} },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      callback = function(event)
+        -- TODO: Replace with keymap_utils
+        local map = function(keys, func, desc, mode)
+          mode = mode or "n"
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+        end
+
+        map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+        map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+        map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+        -- Highlight word occurrences under cursor
+        -- From Kickstart nvim https://github.com/nvim-lua/kickstart.nvim/blob/648471c9eb1f757729e800ff0a6509e01232a8e9/init.lua#L534
+        --
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client:supports_method("textDocument/documentHighlight", event.buf) then
+          local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.document_highlight,
+          })
+
+          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.clear_references,
+          })
+
+          vim.api.nvim_create_autocmd("LspDetach", {
+            group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+            end,
+          })
+        end
+
+        -- Toggle inlay hints
+        --
+        if client and client:supports_method("textDocument/inlayHint", event.buf) then
+          map("<leader>th", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+          end, "[T]oggle Inlay [H]ints")
+        end
+      end,
+    })
+  end,
+}
+return {}
