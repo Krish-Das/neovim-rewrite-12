d2bae0c061098b74797fcf554c9e929ad1cba8f1
Krish-Das <krishdasatgames@gmail.com>
2026-04-16 10:46:48 +0530

    add blink completion

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,26 @@
+return {
+  "saghen/blink.cmp",
+  event = "VimEnter",
+  version = "1.*",
+
+  --- @module 'blink.cmp'
+  --- @type blink.cmp.Config
+  opts = {
+    keymap = { preset = "default" },
+    appearance = { nerd_font_variant = "mono" },
+    completion = {
+      ghost_text = { enabled = true },
+      menu = { auto_show = false },
+      documentation = { auto_show = false },
+    },
+    signature = { enabled = false },
+    sources = {
+      default = { "lsp", "path", "buffer" },
+    },
+    fuzzy = {
+      implementation = "prefer_rust_with_warning",
+      sorts = { "exact", "score", "sort_text" },
+    },
+  },
+  -- opts_extend = { "sources.default" },
+}
