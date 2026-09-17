7602e431d500ce636cea10e34e5a3ec68eb3ac6c
Krish-Das <krish@upsher.dev>
2026-09-17 12:53:58 +0530

    feat(snippets): add modeline autosnippet

---
+++ snippets/all.lua
@@ -0,0 +1,7 @@
+---@diagnostic disable: undefined-global
+return {
+  s(
+    { trig = ";modeline", snippetType = "autosnippet", wordTrig = true },
+    fmta("# vim:fileencoding=utf-8:ft=<>", { i(1) })
+  ),
+}
